9a998d01f0bdbf2c56d487adefd55d9d07eaf4ea
hardikg23 <hardikg23@gmail.com>
2014-12-13 17:08:00 +0530

    category#index page UI change and display sub category

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,11 +2,13 @@ class CategoriesController < ApplicationController
   before_filter :get_bottem_class, :except => []
 
   def index
-    @categories = Category.includes(:videos).where(:category_type => "main")
+    categories = Category.includes(:videos).where(:category_type => "main").order('category_name')
+    @categories = categories.group_by { |c| c.category_name[0] }
   end
 
   def show
     @category = Category.friendly.find_by_slug(params[:id])
+    @sub_category = Category.includes(:videos).where(:category_type => "sub", :parent_category => @category.id).order('category_name')
     per_page = params[:per_page] || 6
     if params[:sort] == 'popular'
       @videos = @category.videos.order('view_count desc').paginate(:page => current_page, :per_page => per_page)
--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -8,27 +8,6 @@ class VideosController < ApplicationController
     else
       @videos = Video.includes(:channel).order('id desc').paginate(:page => current_page, :per_page => per_page)
     end
-    # if params[:categories]
-    #   @category_ids = params[:categories][:category_ids].reject! { |c| c.empty? }
-    #   @parse_categories = @category_ids.join(",")
-    # end
-    # if !params[:category_ids].blank?
-    #   @category_ids = params[:category_ids].split(",").map {|c| c.to_i}
-    #   @parse_categories = @category_ids.join(",")
-    # end
-    # if params[:sort] == 'popular'
-      # if @category_ids.blank?
-        # @videos = Video.includes(:channel).order('view_count desc').paginate(:page => current_page, :per_page => per_page)
-      # else
-      #   @videos = Video.joins(:categories).includes(:channel,).order('view_count desc').where('categories_videos.category_id in (?)',@category_ids).uniq.paginate(:page => current_page, :per_page => per_page)
-      # end
-    # else
-      # if @category_ids.blank?
-        # @videos = Video.includes(:channel).order('id desc').paginate(:page => current_page, :per_page => per_page)
-      # else
-      #   @videos = Video.joins(:categories).includes(:channel,).order('id desc').where('categories_videos.category_id in (?)',@category_ids).uniq.paginate(:page => current_page, :per_page => per_page)
-      # end
-    # end
     respond_to do |format|
       format.html
       format.js
